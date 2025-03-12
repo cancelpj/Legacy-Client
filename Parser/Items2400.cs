a5d587cf661f62071c7acbf4b41bee508b2b1c42
彭俊 <9997452@qq.com>
2025-03-12 13:10:35 +0800

    新增 README

---
+++ Parser/Items2400.cs
@@ -0,0 +1,63 @@
+﻿namespace Parser
+{
+    public class Items2400
+    {
+        /// <summary>
+        /// 纤芯
+        /// </summary>
+        public Items2400Line Core { get; set; }
+
+        /// <summary>
+        /// 包层
+        /// </summary>
+        public Items2400Line[] Clad { get; set; }
+
+        /// <summary>
+        /// 涂敷层1
+        /// </summary>
+        public Items2400Line[] Coat1 { get; set; }
+
+        /// <summary>
+        /// 涂敷层2
+        /// </summary>
+        public Items2400Line[] Coat2 { get; set; }
+
+        /// <summary>
+        /// 应力区1
+        /// </summary>
+        public Items2400Line[] Rod1 { get; set; }
+
+        /// <summary>
+        /// 应力区2
+        /// </summary>
+        public Items2400Line[] Rod2 { get; set; }
+
+        /// <summary>
+        /// 到包层直线度
+        /// </summary>
+        public decimal PlanarityToClad { get; set; }
+
+        /// <summary>
+        /// 到纤芯直线度
+        /// </summary>
+        public decimal PlanarityToCore { get; set; }
+    }
+
+    public class Items2400Line
+    {
+        /// <summary>
+        /// 直径 单位:um
+        /// </summary>
+        public string Diameter { get; set; }
+
+        /// <summary>
+        /// 不圆度 单位:%
+        /// </summary>
+        public string Noncirc { get; set; }
+
+        /// <summary>
+        /// 同心度 单位:um
+        /// </summary>
+        public string Conc { get; set; }
+    }
+}
---
+++ RedisClient/README.md
@@ -0,0 +1,13 @@
+# RedisClient
+
+## 简介
+这是一个可以在 WinXP 系统下工作的 Redis 客户端命令行程序，适合在其他程序中调用。
+
+## 使用方法
+```shell
+RedisClient.exe <command> [args...] [-options...]
+options:
+  -host: Redis 服务器地址
+  -port: Redis 服务器端口
+  -password: Redis 服务器密码
+```
